57f3904c097750979c2356f5c2fe0b80de738266
FylypekUNO <filip.dudzik.uno@gmail.com>
2024-10-27 16:16:50 +0100

    Floating undo button

---
+++ CustomButton1.cs
@@ -0,0 +1,94 @@
+﻿namespace floating_buttons;
+
+public class DragEventArgs : EventArgs
+{
+    public Point LastLocation { get; private set; }
+    public Point CurrentLocation { get; private set; }
+    public int DeltaX { get; private set; }
+    public int DeltaY { get; private set; }
+
+    public DragEventArgs(Point lastLocation, Point currentLocation)
+    {
+        LastLocation = lastLocation;
+        CurrentLocation = currentLocation;
+        DeltaX = currentLocation.X - lastLocation.X;
+        DeltaY = currentLocation.Y - lastLocation.Y;
+    }
+}
+
+public class CustomButton1 : Button
+{
+    private bool isMouseDown = false;
+    private bool isDragging = false;
+
+    private Point lastLocation;
+    private TimeSpan clickTime;
+
+    public event EventHandler DragStart = delegate { };
+    public event EventHandler DragEnd = delegate { };
+    public event EventHandler<DragEventArgs> Drag = delegate { };
+    public event EventHandler NonDragClick = delegate { };
+
+    public CustomButton1(String text)
+    {
+        this.Text = text;
+
+        SetVisuals();
+    }
+
+    private void SetVisuals()
+    {
+        this.FlatStyle = FlatStyle.Flat; // Remove button border
+        this.BackColor = Color.LightBlue; // Initial color
+    }
+
+    protected override void OnMouseDown(MouseEventArgs e)
+    {
+        base.OnMouseDown(e);
+
+        isMouseDown = true;
+        lastLocation = e.Location;
+        clickTime = DateTime.Now.TimeOfDay;
+    }
+
+    protected override void OnMouseUp(MouseEventArgs e)
+    {
+        base.OnMouseUp(e);
+
+        if (isDragging) DragEnd(this, EventArgs.Empty);
+        else NonDragClick(this, EventArgs.Empty);
+
+
+        isMouseDown = false;
+        isDragging = false;
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        base.OnMouseMove(e);
+
+        if (!isMouseDown) return;
+
+        if (!isDragging)
+        {
+            isDragging = CheckIfDragging(e);
+
+            if (isDragging) DragStart(this, EventArgs.Empty);
+            else return;
+        }
+
+        Drag(this, new DragEventArgs(lastLocation, e.Location));
+    }
+
+    private bool CheckIfDragging(MouseEventArgs e)
+    {
+        // Check if the mouse has moved more than 10 pixels
+        var dx = e.Location.X - lastLocation.X;
+        var dy = e.Location.Y - lastLocation.Y;
+
+        // Check if the mouse has been held for more than 100ms
+        var timediff = DateTime.Now.TimeOfDay - clickTime;
+
+        return dx + dy > 10 || timediff.TotalMilliseconds > 100;
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,71 @@
+using System.Runtime.InteropServices;
+
 namespace floating_buttons;
 
 public partial class Form1 : Form
 {
-    public Form1()
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    private const int GWL_EXSTYLE = -20; // Extended window styles
+    private const int WS_EX_LAYERED = 0x80000; // Allows transparency
+    private const int WS_EX_TOOLWINDOW = 0x80; // Hides from Alt+Tab
+    private const int WS_EX_NOACTIVATE = 0x8000000; // Prevents activation on click
+
+    private CustomButton1 myButton;
+
+    public String label { get; private set; }
+    public String keystroke { get; private set; }
+
+    public Form1(String label, String keystroke)
     {
-        InitializeComponent();
+        this.label = label;
+        this.keystroke = keystroke;
+
+        this.Load += OnLoad;
+
+        InitilizeComponent();
+        InitializeButton();
+    }
+
+    private void InitilizeComponent()
+    {
+        this.SuspendLayout();
+        this.ClientSize = new Size(200, 200);
+        this.Name = "Form1";
+        this.ResumeLayout(false);
+    }
+
+    private void InitializeButton()
+    {
+        myButton = new CustomButton1(label);
+
+        myButton.Drag += (sender, e) =>
+        {
+            this.Left += e.DeltaX;
+            this.Top += e.DeltaY;
+        };
+
+        myButton.NonDragClick += (sender, e) => { SendKeys.SendWait(keystroke); };
+
+        this.Controls.Add(myButton);
+    }
+
+    private void OnLoad(object? sender, EventArgs e)
+    {
+        this.ShowInTaskbar = false; // Hide from taskbar
+        this.FormBorderStyle = FormBorderStyle.None; // Remove window borders
+        this.TopMost = true; // Keep the form on top
+        this.WindowState = FormWindowState.Normal; // Set normal state
+        this.BackColor = Color.Lime; // Set a background color (used for transparency)
+        this.TransparencyKey = Color.Lime; // Make this color transparent
+        this.AllowTransparency = true; // Enable transparency
+
+        // Set the window styles to hide it and allow interaction
+        var exStyle = GetWindowLong(this.Handle, GWL_EXSTYLE);
+        SetWindowLong(this.Handle, GWL_EXSTYLE, exStyle | WS_EX_LAYERED | WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE);
     }
 }
